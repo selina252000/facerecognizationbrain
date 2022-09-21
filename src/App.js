34794cb8730c7e736d9705f484727f87139a3cb0
Selina252000 <selina.darnal@netsmartz.net>
2022-09-21 18:02:31 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React ,{Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      {/* <Logo/>
+      <ImageLinkForm/>
+      < FAceRecognition/> */}
+
+      
+    </div>
+  );
+}
+
+export default App;
